5aab15f7861e359640264a7475bae459fd3676e0
Talha Anjum <34774080+talha12981@users.noreply.github.com>
2022-04-06 23:46:03 +0500

    Add files via upload

---
+++ TodoPHP-master/app/controllers/HomeController.php
@@ -0,0 +1,47 @@
+<?php
+
+class HomeController extends BaseController {
+
+	/*
+	|--------------------------------------------------------------------------
+	| Default Home Controller
+	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
+	*/
+
+	public function showWelcome()
+	{
+		return View::make('hello');
+	}
+
+	public function saveTodo()
+	{
+		// save new todo
+		if(Input::get('inputTodo') != null || Input::get('inputTodo') != "")
+			{
+				$d=strtotime(Input::get('inputDuedate'));
+				$todo = new Todo;
+				$todo->title = Input::get('inputTodo');
+				$todo->description = Input::get('inputDescription');
+				$todo->complete = false;
+				$todo->duedate = date("Y-m-d h:i:s", $d);
+				$todo->author = Input::get('inputAuthor');
+				$todo->tags = Input::get('inputTags');
+				$todo->save();
+			}
+
+		$todos = Todo::all();
+
+		// todo: use a redirect instead of the line below to
+		// avoid persisting the same data if user hits the 
+		// refresh button
+		return View::make('todos')->with('todos', $todos);
+	}
+
+}
